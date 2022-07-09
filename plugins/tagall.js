1c791af54b2e52f4f0d84e4f1d7d10c54e1790a3
Deffnohy <108708966+Deffnohy@users.noreply.github.com>
2022-07-10 02:00:44 +0700

    Add files via upload

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,8 +1,8 @@
 
 let handler = async(m, { conn, text, participants }) => {
-  let teks = `${text ? text : ' '}\n\n`
+  let teks = `✾𝑴𝑬𝑺𝑺𝑨𝑮𝑬✾ ${text ? text : ' '}\n\n╔─── ᚛۞ 𝐓𝐀𝐆 𝐀𝐋𝐋 ۞᚜ ───╗\n`
 		      	for (let mem of participants) {
-		            teks += `@${mem.id.split('@')[0]}\n`
+		            teks += `╠➣@${mem.id.split('@')[0]}\n`
 				}
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
